df4e8d12d53f8137164e0f92bb68b68c5eaa6eda
liaoshiqiang <liaoshiqiang@kuaishou.com>
2024-09-27 16:35:25 +0800

    把MainComp直接移到MainView

--- apps/mylog-vite-ssr/src/views/MainView/index.vue
+++ apps/mylog-vite-ssr/src/views/MainView/index.vue
@@ -2,15 +2,29 @@
  主要页面的容器，包含上方导航栏，下面的链接栏，中间主体部分。
 -->
 <script setup lang="ts">
-import MainComp from "./comps/MainComp.vue";
+import HeaderComp from "./comps/HeaderComp.vue";
+import OneSentence from "./comps/Lans/OneSentence.vue";
 </script>
 
 <template>
   <div class="main-view">
-    main-view
-
-    <!-- <HeaderComp /> -->
-    <MainComp />
+    <HeaderComp />
+    <div class="main-comp">
+      <div class="center">
+        <div class="middle">
+          <RouterView />
+        </div>
+        <div class="left">
+          <!-- <UserLan /> -->
+          <!-- <NoteLan /> -->
+        </div>
+        <div class="right">
+          <!-- 目录栏 -->
+          <!-- <ContentLan /> -->
+          <OneSentence />
+        </div>
+      </div>
+    </div>
     <!-- <FooterComp /> -->
   </div>
 </template>
@@ -19,5 +33,56 @@ import MainComp from "./comps/MainComp.vue";
 .main-view {
   position: relative;
   min-height: 100vh;
+
+  .main-comp {
+    display: flex;
+    justify-content: center;
+
+    .center {
+      width: var(--center-width);
+      display: flex;
+      gap: var(--gap);
+      transition: width 0.5s;
+
+      .middle {
+        width: 0;
+        order: 2;
+        flex: 1;
+      }
+
+      .left {
+        order: 1;
+        width: var(--lan-width);
+
+        // 栏固定
+        > div:nth-child(1) {
+          position: sticky;
+          top: calc(var(--header-height) + var(--gap));
+        }
+      }
+
+      .right {
+        order: 3;
+        width: var(--lan-width);
+      }
+
+      .left,
+      .right {
+        display: flex;
+        flex-direction: column;
+        gap: var(--gap);
+        > * {
+          border-radius: var(--border-radius);
+        }
+      }
+
+      @media (max-width: 890px) {
+        .left,
+        .right {
+          display: none;
+        }
+      }
+    }
+  }
 }
 </style>
