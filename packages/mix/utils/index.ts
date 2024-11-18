628f21b0395ec62bb014b81d7d384094b87a3da7
bit-ts <qu.bit@qq.com>
2024-11-19 01:30:17 +0800

    feat: 设置mix包，放杂七杂八的模块

---
+++ packages/mix/utils/index.ts
@@ -0,0 +1,30 @@
+import type { log as LogPO } from '@prisma/client'
+import { LogVO } from '../types'
+import dayjs from 'dayjs'
+
+/**
+ * 数据库转VO对象，主要后端用
+ * PO to VO :
+ *   1. JsonValue 转 对象
+ *   2. Date 转 dayjs
+ * @param log PO数据库对象 trpc直接返回的对象类型
+ * @returns
+ */
+export function toLogVO4PO(log: LogPO): LogVO {
+  const logVO: LogVO = {
+    ...log,
+    userid: log.userid || undefined,
+    type: log.type as LogVO['type'],
+    logtime: dayjs(log.logtime),
+    sendtime: dayjs(log.sendtime),
+    tags: log.tags as LogVO['tags'],
+    imgs: log.imgs as LogVO['imgs'],
+    videos: log.videos as LogVO['videos'],
+    audios: log.audios as LogVO['audios'],
+    files: log.files as LogVO['files'],
+    location: log.location as LogVO['location'],
+    people: log.people as LogVO['people'],
+    info: log.info as LogVO['info'],
+  }
+  return logVO
+}
