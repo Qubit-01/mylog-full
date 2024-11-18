fde7ef71f7fb2b02756a8280125468cec9b3bb49
liaoshiqiang <liaoshiqiang@kuaishou.com>
2024-11-18 19:03:51 +0800

    feat: Nest支持https

--- apps/mylog-nest/src/main.ts
+++ apps/mylog-nest/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('./cert/mylog.cool.key'),
+      cert: fs.readFileSync('./cert/mylog.cool.crt'),
+    },
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
