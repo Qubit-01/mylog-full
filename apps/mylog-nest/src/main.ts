6e8f5eda3ea23146103d92d922785ecd2e9ac52b
liaoshiqiang <liaoshiqiang@kuaishou.com>
2024-11-19 22:57:48 +0800

    feat: 跨域和自动转换入参设置

--- apps/mylog-nest/src/main.ts
+++ apps/mylog-nest/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
@@ -8,7 +9,9 @@ async function bootstrap() {
       key: fs.readFileSync('./cert/mylog.cool.key'),
       cert: fs.readFileSync('./cert/mylog.cool.crt'),
     },
+    cors: { origin: ['https://mylog.cool', 'https://www.mylog.cool'] },
   });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
