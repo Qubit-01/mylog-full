c29bcf03c26d6bea9531c2b21b33dfadfc20dbc7
bit-ts <qu.bit@qq.com>
2025-01-19 18:19:42 +0800

    feat: https设置

--- apps/mylog-nuxt/nuxt.config.ts
+++ apps/mylog-nuxt/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  devServer: {
+    host: 'mylog.cool',
+    port: 443,
+    https: {
+      key: './cert/mylog.cool.key',
+      cert: './cert/mylog.cool.crt',
+    },
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/base.scss'],
   vite: {
