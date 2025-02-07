304d54e258f485c2d10d25ebfe3a46ed9e4661e9
liaoshiqiang <liaoshiqiang@kuaishou.com>
2025-02-07 22:28:03 +0800

    feat: 有个水合bug

--- apps/mylog-nuxt/pages/mylog/index.vue
+++ apps/mylog-nuxt/pages/mylog/index.vue
@@ -13,24 +13,13 @@ const Mylog = useMylogStore()
       v-infinite-scroll="Mylog.addLogs"
       :infinite-scroll-disabled="Mylog.status !== 'success'"
     >
-      <!-- 编辑预览 -->
-      <!-- <el-timeline-item
-      v-if="logReleaseDom?.logEdit.content"
-      timestamp="编辑预览"
-      placement="top"
-    >
-      <LogMylog :log="logReleaseDom?.logEdit" />
-    </el-timeline-item> -->
-
       <!-- 时间线开始 -->
       <template v-for="(log, i) in Mylog.logs" :key="log.id">
-        <!-- {{ log }} -->
-        <!-- {{ dayjs(dayjs(log.logtime)) }} -->
         <!-- 年份节点 -->
-        <el-timeline-item
+        <ElTimelineItem
           v-if="
             i == 0 ||
-            !dayjs(log.logtime).isSame(Mylog.logs[i - 1].logtime!, 'year')
+            !dayjs(log.logtime).isSame(Mylog.logs[i - 1].logtime, 'year')
           "
           :timestamp="dayjs(log.logtime).year().toString()"
           type="success"
@@ -39,57 +28,23 @@ const Mylog = useMylogStore()
         />
 
         <!-- 日期节点 -->
-        <el-timeline-item
+        <ElTimelineItem
           v-if="
             i == 0 ||
-            !dayjs(log.logtime)!.isSame(Mylog.logs[i - 1].logtime!, 'day')
+            !dayjs(log.logtime).isSame(Mylog.logs[i - 1].logtime, 'day')
           "
-          :timestamp="dayjs(log.logtime)!.format('YYYY-MM-DD')"
+          :timestamp="dayjs(log.logtime).format('YYYY-MM-DD')"
           placement="top"
-        >
-          <!-- closable @close="tabNoteClose(tag)" -->
-          <!-- <div class="tags">
-            <ElTag
-              v-for="tag in Tags.listAll.filter((tag) =>
-                tag.logtime.isSame(dayjs(log.logtime), 'day'),
-              )"
-              :key="tag.content"
-            >
-              {{ tag.content }}
-            </ElTag>
-          </div> -->
-        </el-timeline-item>
+        />
 
         <!-- Log节点  :color="log.type === 'public' ? 'var(--el-color-warning)' : 'transparent'"-->
-        <el-timeline-item hide-timestamp center color="transparent">
-          <!-- <LogMylog :log="log" /> -->
-          <Log :key="log.id" :log="log" />
-        </el-timeline-item>
+        <ElTimelineItem hide-timestamp center color="transparent">
+          <Log :id="log.id" :log="log" />
+        </ElTimelineItem>
+        <!-- {{ log }} -->
       </template>
 
-      <!-- 没有数据 -->
-      <!-- <el-timeline-item
-        v-if="!Mylog.list.length && !Mylog.loading"
-        timestamp="没有数据哦~"
-        placement="top"
-      >
-        <div
-          v-m
-          style="padding: var(--padding); border-radius: var(--border-radius)"
-        >
-          没有数据哦~
-        </div>
-      </el-timeline-item> -->
-
-      <!-- <el-timeline-item
-        v-if="Mylog.loading"
-        timestamp="loading..."
-        placement="top"
-      >
-        <LogLoading />
-      </el-timeline-item> -->
-
-      <!-- <el-timeline-item timestamp="origin" placement="top" /> -->
+      <ElTimelineItem timestamp="origin" placement="top" />
     </el-timeline>
   </div>
 </template>
