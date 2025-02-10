d91a6a1995571611472aca8a0818d6e7c5e7d735
liaoshiqiang <liaoshiqiang@kuaishou.com>
2025-02-10 14:46:43 +0800

    feat: nest接口似乎只能二选一？

--- apps/mylog-nest/src/test/test.controller.ts
+++ apps/mylog-nest/src/test/test.controller.ts
@@ -8,9 +8,19 @@ export class TestController {
    * @returns "Hello World!"
    */
   @Get('hello')
-  @Post('hello')
   hello(@Cookies() cookies: string) {
-    console.log('🐤 Hello World!');
+    console.log('🐤 Hello World! get');
+    console.log('🐤 Cookies', cookies);
+    return '🐤 Hello World!';
+  }
+
+  /**
+   * 测试后端服务是否正常
+   * @returns "Hello World!"
+   */
+  @Post('hello-post')
+  helloPost(@Cookies() cookies: string) {
+    console.log('🐤 Hello World! post');
     console.log('🐤 Cookies', cookies);
     return '🐤 Hello World!';
   }
