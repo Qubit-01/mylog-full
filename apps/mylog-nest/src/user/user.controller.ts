b9af066429313cd80be3d07ed05bfa2ca376a97e
bit <2928318801@qq.com>
2025-02-17 00:47:50 +0800

    feat: 用户设置覆盖算法有待优化

--- apps/mylog-nest/src/user/user.controller.ts
+++ apps/mylog-nest/src/user/user.controller.ts
@@ -4,6 +4,8 @@ import { getUseridByPswd } from '@prisma/client/sql';
 import { PrismaClient } from '@prisma/client';
 import { sign } from 'src/utils/jwt';
 import { Userid } from 'src/utils';
+import { type UserVO } from '@mylog-full/mix/types';
+import dayjs from 'dayjs';
 
 @Controller('user')
 export class UserController {
@@ -46,15 +48,45 @@ export class UserController {
   ) {
     console.log('🐔 get_user: ', userid, body);
 
+    let user;
     if ('id' in body) {
-      return this.prisma.user.findUnique({
-        where: { userid },
-      });
+      user = this.prisma.user.findUnique({ where: { userid } });
     } else if ('name' in body) {
-      return this.prisma.user.findUnique({ where: { name: body.name } });
+      user = this.prisma.user.findUnique({ where: { name: body.name } });
     } else if (userid) {
-      return this.prisma.user.findUnique({ where: { userid } });
+      user = this.prisma.user.findUnique({ where: { userid } });
     }
+
+    const userInit: UserVO = {
+      id: 0,
+      name: '',
+      img: '',
+      info: {},
+      setting: {
+        page: {
+          theme: 'light',
+        },
+        mylog: {
+          tags: [],
+          filters: [],
+          filterIndex: 0,
+          calendarTags: [],
+        },
+        map: {
+          diyPoints: [],
+        },
+      },
+      createtime: dayjs().valueOf(),
+    } as const;
+
+    // 覆盖算法有待优化
+    const setting = {
+      page: { ...userInit.setting.page, ...user.setting.page },
+      mylog: { ...userInit.setting.mylog, ...user.setting.mylog },
+      map: { ...userInit.setting.map, ...user.setting.map },
+    };
+
+    return { ...userInit, ...user, setting: setting };
   }
 
   /**
