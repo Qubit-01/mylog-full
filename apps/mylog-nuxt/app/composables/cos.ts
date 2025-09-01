29f4e7825fe72c3651327c6bab0270a2141c8fc9
liaoshiqiang <liaoshiqiang@kuaishou.com>
2025-09-01 21:52:11 +0800

    feat: fetch要加credentials才能带cookie

--- apps/mylog-nuxt/app/composables/cos.ts
+++ apps/mylog-nuxt/app/composables/cos.ts
@@ -11,24 +11,26 @@ const cos = new COS({
    */
   getAuthorization(options, callback) {
     // 获取临时密钥接口 API
-    fetch(baseURL + '/cos/get_credential', { method: 'GET' }).then(
-      async (res) => {
-        const data = await res.json()
-        console.log('LSQ> /cos/get_credential: ', data)
-        if (!data) {
-          console.error('凭证无效:\n' + JSON.stringify(data, null, 2))
-          return
-        }
-        callback({
-          TmpSecretId: data.credentials.tmpSecretId,
-          TmpSecretKey: data.credentials.tmpSecretKey,
-          SecurityToken: data.credentials.sessionToken,
-          // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
-          StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
-          ExpiredTime: data.expiredTime, // 时间戳，单位秒，如：1580000000
-        })
-      },
-    )
+    fetch(baseURL + '/cos/get_credential', {
+      method: 'GET',
+      credentials: 'include',
+    }).then(async (res) => {
+      console.log('LSQ> ', res);
+      const data = await res.json()
+      console.log('LSQ> /cos/get_credential: ', data)
+      if (!data) {
+        console.error('凭证无效:\n' + JSON.stringify(data, null, 2))
+        return
+      }
+      callback({
+        TmpSecretId: data.credentials.tmpSecretId,
+        TmpSecretKey: data.credentials.tmpSecretKey,
+        SecurityToken: data.credentials.sessionToken,
+        // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+        StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+        ExpiredTime: data.expiredTime, // 时间戳，单位秒，如：1580000000
+      })
+    })
   },
 })
 
