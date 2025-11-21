0a5f1afc7e9748ea2b8d13476fbc2f31b40448e5
liaoshiqiang <liaoshiqiang@kuaishou.com>
2025-11-21 15:22:15 +0800

    feat: 删除H5子项目

--- apps/mylog-h5/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-declare module 'element-plus/dist/locale/zh-cn.mjs'
-declare module 'viewerjs'
-declare module 'lodash'
-
-declare interface Window {
-  QC: QQConnect
-}
