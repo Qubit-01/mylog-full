3c0844f1142eaaf381c28392e58df0c6fe38345b
liaoshiqiang <liaoshiqiang@kuaishou.com>
2025-11-21 15:24:44 +0800

    feat: 删除trpc-prisma子项目

--- apps/mylog-trpc-prisma/prisma/sql/getUserByPswd.sql
+++
@@ -1,5 +0,0 @@
--- @param {String} $1:name 用户名
--- @param {String} $2:pswd 密码
-SELECT `id` userid 
-FROM `user` 
-WHERE name=? AND pswd=SHA(?)
--- apps/mylog-trpc-prisma/src/utils/trpc.ts
+++
@@ -1,8 +0,0 @@
-import { initTRPC } from "@trpc/server";
-
-/** 初始化TRPC后端，并且保证是单例模式（只导出方法） */
-const t = initTRPC.create();
-
-/** 导出路由和过程调用 */
-export const router = t.router;
-export const publicProcedure = t.procedure;
