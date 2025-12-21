82f7d693430cfa9dc75039f4ff659c951d187d03
bit <liaoshiqiang@kuaishou.com>
2025-12-21 19:16:46 +0800

    feat: 优化

--- apps/mylog-be/src/main.ts
+++ apps/mylog-be/src/main.ts
@@ -3,21 +3,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
+const origin = [
+  'https://mylog.ink',
+  'https://www.mylog.ink',
+  'http://mylog.ink',
+  'http://www.mylog.ink',
+  'http://localhost',
+]
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'https://mylog.ink',
-        'https://www.mylog.ink',
-        'http://mylog.ink',
-        'http://www.mylog.ink',
-        'http://localhost',
-      ],
-      credentials: true,
-    },
+    cors: { origin, credentials: true },
   });
   app.use(cookieParser()); // 解析cookie
-  app.useGlobalPipes(new ValidationPipe({ transform: true })); // 自动转换参数类型，错误直接400
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // 全局验证器，自动转换参数类型，错误直接400
   await app.listen(process.env.PORT ?? 20914);
 }
 bootstrap();
