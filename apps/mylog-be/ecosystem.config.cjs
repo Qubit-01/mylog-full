9c7ddf7f4f5fd42b51baa9719a40468a84cc0d0f
bit <liaoshiqiang@kuaishou.com>
2026-01-03 12:17:07 +0800

    feat: nest 11 把入口放进src了

--- apps/mylog-be/ecosystem.config.cjs
+++ apps/mylog-be/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'mylog-be',
-      script: 'dist/main.js', // 入口路径
+      script: 'dist/src/main.js', // 入口路径
       // instances: '1', // 根据需求设置实例数量
       // exec_mode: 'cluster',
       autorestart: true,
