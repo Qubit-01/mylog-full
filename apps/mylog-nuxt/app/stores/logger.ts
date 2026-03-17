6b532e998e66ba4121b68e7792f9d5834313f143
bit <liaoshiqiang@kuaishou.com>
2026-03-17 13:37:38 +0800

    feat: 修复用userid 0

--- apps/mylog-nuxt/app/stores/logger.ts
+++ apps/mylog-nuxt/app/stores/logger.ts
@@ -1,12 +1,21 @@
+/** 每页的条数 */
+const limit = 20
+
 export const useLoggerStore = defineStore('logger', () => {
   const { user } = refsGlobalStore()
   const logs = ref<Log[]>([])
-  const params = reactive({ skip: 0, limit: 20, userid: user.value.id })
+  const skip = ref(0)
+  const params = computed(() => ({
+    skip: skip.value,
+    limit,
+    userid: user.value.id,
+  }))
   const noMore = ref(false)
 
   // 每次触发请求，都会自动 push 在 logs 最后
   const { data, status, refresh } = useFetchApi<Log[]>('/log/get_publics', {
     key: 'logger',
+    immediate: false,
     headers: { Cookie: `token=${useCookie('token').value}` },
     body: params,
   })
@@ -14,13 +23,13 @@ export const useLoggerStore = defineStore('logger', () => {
   watch(data, (logsRes) => {
     if (!logsRes) return
     logs.value.push(...logsRes)
-    if (logsRes.length < params.limit) noMore.value = true
+    if (logsRes.length < limit) noMore.value = true
   })
 
   /** 请求下一页数据 */
   const fetchLogs = () => {
     if (noMore.value) return
-    params.skip += params.limit
+    skip.value += limit
   }
 
   return {
